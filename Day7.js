2ddf36a54eae98bbbba4689c46fa7edb40754911
Rakesh Roshan <roshanrakesh7362@gmail.com>
2023-11-12 23:47:08 +0530

    Challenge 7

---
+++ Day7.js
@@ -0,0 +1,11 @@
+// Challenge 7
+
+function checkNumbers(num1, num2) {
+    return num1 === 50 || num2 === 50 || num1 + num2 === 50;
+}
+
+console.log(checkNumbers(25, 25));
+console.log(checkNumbers(50, 10));
+console.log(checkNumbers(20, 30));
+console.log(checkNumbers(15, 35));
+console.log(checkNumbers(40, 30));
